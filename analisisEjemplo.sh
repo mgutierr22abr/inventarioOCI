45ab0c1d56bd8024c00d655aad2e0aa95a11e934
Martín Gutiérrez <martin.gutierrez@oracle.com>
2020-12-18 14:17:41 -0300

    Normal

--- analisisEjemplo.sh
+++ analisisEjemplo.sh
@@ -4,6 +4,8 @@ if [ a$1 == a ]; then
 else
 	T=$1
 fi
+echo "############## detalle"
+cat $T | jq '.resources[].instances[].attributes' | grep ocid1 | grep '"id"' | awk -F'"' '{print $4}' | sort -u
 echo "############## tenancy"
 cat $T | jq -r '.outputs.tenancy.value.name'
 echo "############## AD"
@@ -11,8 +13,6 @@ cat $T | jq -r '.outputs.ads.value.availability_domains[].name'
 echo "############## compartments activos "
 cat $T | jq -r '.outputs.compartments.value.compartments[] | select( .state == "ACTIVE" ) | .name'
 echo "############## computo"
-cat $T | jq -r '.outputs.oci_core_instances.value[].instances[] | .display_name+" "+.region+" "+.availability_domain+" "+.shape+" "+.state'
+cat $T | |  jq -r '.outputs.resumen.value[] | .display_name+","+.availability_domain+","+.state+","+.public_ip+","+.private_ip'
 echo "############## db-system"
 cat $T | jq '.outputs.oci_database_db_systems.value[].db_systems[] | .display_name+" "+.availability_domain+" "+.shape+" "+(.node_count|tostring)+" "+(.data_storage_size_in_gb|tostring)+" "+.database_edition'
-echo "############## detalle"
-cat $T | jq '.resources[].instances[].attributes' | grep ocid1 | grep '"id"' | awk -F'"' '{print $4}' | sort -u
